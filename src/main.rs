237a107710eed8e7d96e0cd95644d6c4742f225d
sp-ino <spinovale97@gmail.com>
2022-10-31 10:55:58 +0100

    Rename the struct as Ffix, add comments and implement associated function for converting one fixed point type into another

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ use std::ops;
 
 
 fn quantize_fix(x: f64, w: u32, f: u32) -> f64 {
+    // This function quantizes a float number as a fixed point
+    // signed number with word length w and fraction length f.
+    // The rounding method is floor. 
+    // quantize_fix() is used by the Ffix structure to implement
+    // fixed point operators.
+    // Other options will be implemented in the future, such
+    // as the possibility to set the signedness and the rounding
+    // method.
 
     if f >= w {
         println!("error:quantize_fix:the number of fractional bits should
@@ -19,22 +27,48 @@ be strictly less than the word length. I'm not performing quantization.");
 
 
 #[derive(Debug, Clone, Copy)]
-struct Fix {
+struct Ffix {
+    // This struct allows to represent signed fixed point
+    // numbers with floor rounding method. For the moment,
+    // the user can only choose the word length and the
+    // fraction length of the fixed point number. Other
+    // features will be implemented in the future, such
+    // as the possibility to represent unsigned numbers
+    // and a precise rule for operations between numbers
+    // with different word and fraction length.
     value: f64,
     word_bits: u32,
     frac_bits: u32,
 }
 
-impl Fix  {
-    pub fn new(v: f64, word_bits: u32, frac_bits: u32) -> Self {
-         Fix {
-            value: quantize_fix(v, word_bits, frac_bits),
+impl Ffix  {
+    fn new(val: f64, word_bits: u32, frac_bits: u32) -> Self {
+        // This associated function is a constructor for
+        // creating new instances of Ffix.
+         Ffix {
+            value: quantize_fix(val, word_bits, frac_bits),
             word_bits,
             frac_bits,
         }       
     }
 
-    fn pow(&self, exponent: i32) -> Fix {
+    fn from(other: Ffix, word_bits: u32, frac_bits: u32) -> Ffix {
+        // This associated function converts an instance
+        // to another instance with specified word lenght
+        // and fraction length. Note that the instance that
+        // is passed to this method is moved into the
+        // converted instance so it cannot be used anymore.
+
+        Ffix {
+            value: quantize_fix(other.value, word_bits, frac_bits),
+            word_bits,
+            frac_bits,            
+        }
+    }
+
+    fn pow(&self, exponent: i32) -> Ffix {
+        // This method implements fixed point exponentiation.
+
         let val = self.value;
         let mut result = self.value;
 
@@ -44,7 +78,7 @@ impl Fix  {
                                 self.frac_bits);
         }
 
-        Fix {
+        Ffix {
             value: result,
             word_bits: self.word_bits,
             frac_bits: self.frac_bits,
@@ -52,7 +86,9 @@ impl Fix  {
     }
 }
 
-impl ops::Add for Fix {
+// The methods that follow implement overloaded arithmetic operations
+// for fixed point numbers
+impl ops::Add for Ffix {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
@@ -66,7 +102,7 @@ impl ops::Add for Fix {
     }
 }
 
-impl ops::Sub for Fix {
+impl ops::Sub for Ffix {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self {
@@ -80,7 +116,7 @@ impl ops::Sub for Fix {
     }
 }
 
-impl ops::Mul for Fix {
+impl ops::Mul for Ffix {
     type Output = Self;
 
     fn mul(self, other: Self) -> Self {
@@ -94,7 +130,7 @@ impl ops::Mul for Fix {
     }
 }
 
-impl ops::Div for Fix {
+impl ops::Div for Ffix {
     type Output = Self;
 
     fn div(self, other: Self) -> Self {
@@ -110,8 +146,8 @@ impl ops::Div for Fix {
 
 
 fn main() {
-    let a = Fix::new(2.12345678, 18, 12);
-    let b = Fix::new(6.87654321, 18, 12);
+    let a = Ffix::new(2.12345678, 18, 12);
+    let b = Ffix::new(6.87654321, 18, 12);
     
     let x: f64 = 2.12345678;
     let y: f64 = 6.87654321;
