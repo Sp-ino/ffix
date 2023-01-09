715023c58645fa21e45efd69b10c790c7f96b8ab
sp-ino <spinovale97@gmail.com>
2023-01-09 18:40:02 +0100

    Write a first working RangeAnalyzer.

--- src/analysis.rs
+++ src/analysis.rs
@@ -9,10 +9,8 @@
 // Copyright (c) 2022 Valerio Spinogatti
 // Licensed under GNU license
 
-// use crate::types::Ffix;
-// use crate::types::internal::Range;
-
 use crate::types::Ffix;
+use crate::types::quantization;
 
 
 
@@ -20,6 +18,7 @@ pub struct RangeAnalyzer<const S: bool, const W: u32, const F: u32, const R: cha
     variables: Vec<Ffix<S, W, F, R>>,
     names: Vec<String>,
 }
+// use crate::types::Ffix;
 
 
 impl<const S: bool, const W: u32, const F: u32, const R: char> RangeAnalyzer<S, W, F, R> {
@@ -35,8 +34,24 @@ impl<const S: bool, const W: u32, const F: u32, const R: char> RangeAnalyzer<S, 
     }
 
     pub fn log(&self) {
+        println!("Name\t\t\tMinimum\t\t\tMaximum\t\t\tPercentage");
+        
         for (idx, var) in self.variables.iter().enumerate() {
-            println!("Range of {}: {}", self.names[idx], var.range().upper)
+            let uplim;
+            let lowlim;
+            (_, uplim, lowlim) = quantization::compute_lims(var.signed(),
+                                                            var.word_bits(),
+                                                            var.frac_bits(),
+                                                            2);
+            
+            let allowed_width = uplim - lowlim;
+            let actual_width = var.range().width();
+            let percentage_range = 100.0 * actual_width / allowed_width;
+            println!("{}{:24.2}{:24.2}{:24.2}",
+                    self.names[idx],
+                    var.range().lower,
+                    var.range().upper,
+                    percentage_range);
         }
     }
 }
